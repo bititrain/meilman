d06d21fbd90818e2326c6f34a7887bae03cc0799
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2017-09-17 14:48:35 -0500

    MeilMan 1.0.0

--- src/meil-man.ts
+++ src/meil-man.ts
@@ -1,11 +1,27 @@
-export class MeilMan {
-  private greeting: string;
+import TelegramBot = require('node-telegram-bot-api');
 
-  constructor(message: string) {
-    this.greeting = message;
+export class MeilMan {
+  private bot: TelegramBot;
+
+  constructor(private token: string, private url?: string) {
+    this.bot = new TelegramBot(token);
+    if (!url) {
+      this.bot.startPolling();
+    } else {
+      this.bot.setWebHook(`${url}/bot${token}`);
+    }
+    this.configureMeilHandlers();
   }
 
-  public greet(): string {
-    return `Bonjour, ${this.greeting}!`;
+  public processUpdate(update: any): void {
+    this.bot.processUpdate(update);
+  }
+
+  private configureMeilHandlers(): void {
+    const bot = this.bot;
+
+    bot.on('message', msg =>
+      bot.sendMessage(msg.chat.id, "I've just got a meil!")
+    );
   }
 }
